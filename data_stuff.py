be469cc5059816e894acd4a306ca48c443ffdeb2
rhit-naikv <78188260+sumosour@users.noreply.github.com>
2021-12-09 20:43:29 -0500

    did some pandas stuff

---
+++ data_stuff.py
@@ -0,0 +1,26 @@
+from numpy import int64
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# importing the CANLOG09 csv file
+df = pd.read_csv('CANLOG09.csv')
+
+df[' B0'] = df[' B0'].apply(int, base=16)
+df[' B1'] = df[' B1'].apply(int, base=16)
+df[' B2'] = df[' B2'].apply(int, base=16)
+df[' B3'] = df[' B3'].apply(int, base=16)
+df[' B4'] = df[' B4'].apply(int, base=16)
+df[' B5'] = df[' B5'].apply(int, base=16)
+
+b16 = lambda x: int(str(x),16)
+
+df[' B6'] = df[' B6'].apply(b16)
+df[' B7'] = df[' B7'].apply(b16)
+
+df = df.set_index('Millis')
+
+plt.plot(df.loc[df[' ID'] == '534', ' B0'])
+plt.ylabel('B0')
+plt.xlabel('Milliseconds')
+plt.title('534')
+plt.show()
